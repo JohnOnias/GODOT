7449a6736b7af665967f5c7c57b5952d82d90741
JohnOnias <119425650+JohnOnias@users.noreply.github.com>
2024-03-12 19:15:17 -0300

    novo

---
+++ novo projeto/CharacterBody2D.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+
+
+func _on_area_2d_body_entered(body):
+	$Sprite2D.visible = true
+	print($Sprite2D.visible)
+
+
+func _on_area_2d_body_exited(body):
+	$Sprite2D.visible = false
